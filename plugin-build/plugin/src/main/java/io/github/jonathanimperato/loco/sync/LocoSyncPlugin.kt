39a38fcaf12ba0fb59b97c9433fbb425ef0d1a7b
Jonathan Imperato <jonny99dj@gmail.com>
2022-12-19 10:22:04 +0100

    Fixes

--- plugin-build/plugin/src/main/java/io/github/jonathanimperato/loco/sync/LocoSyncPlugin.kt
+++ plugin-build/plugin/src/main/java/io/github/jonathanimperato/loco/sync/LocoSyncPlugin.kt
@@ -8,8 +8,7 @@ const val EXTENSION_NAME = "LocoSync"
 abstract class LocoSyncPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val extension = project.extensions.create(EXTENSION_NAME, Extension::class.java, project)
-        val taskName = project.extensions.taskName
-        project.tasks.register(taskName, GenerateResourcesTask::class.java) {
+        project.tasks.register(extension.taskName.get(), GenerateResourcesTask::class.java) {
             it.resDir.set(extension.resDir)
             it.configs.set(extension.configs)
         }
