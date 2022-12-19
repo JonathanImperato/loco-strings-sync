745cf64d92dd9b54ef803651e3a29a3beafff3c7
Jonathan Imperato <jonny99dj@gmail.com>
2022-12-19 12:42:45 +0100

    Updated

--- plugin-build/plugin/src/main/java/io/github/jonathanimperato/loco/sync/LocoSyncPlugin.kt
+++ plugin-build/plugin/src/main/java/io/github/jonathanimperato/loco/sync/LocoSyncPlugin.kt
@@ -4,11 +4,11 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 const val EXTENSION_NAME = "LocoSync"
-
+const val TASK_NAME = "LocoStringSync"
 abstract class LocoSyncPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val extension = project.extensions.create(EXTENSION_NAME, Extension::class.java, project)
-        project.tasks.register(extension.taskName.get(), GenerateResourcesTask::class.java) {
+        project.tasks.register(extension.taskName.getOrElse(TASK_NAME), GenerateResourcesTask::class.java) {
             it.resDir.set(extension.resDir)
             it.configs.set(extension.configs)
         }
